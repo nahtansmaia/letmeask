16f1be173d09b1cc039b167d76379d0b2e68021e
Nathan Maia <nahtansmaia@gmail.com>
2021-06-24 19:35:53 -0300

    Ajuste in github
    Ajuste in github

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
 import { AdminRoom } from './pages/AdminRoom';
 import { Home } from './pages/Home';
