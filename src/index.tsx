2f07c1a12b90deefbd68e9286d0c0f6f19c59446
Nathan Maia <nahtansmaia@gmail.com>
2021-06-28 14:34:05 -0300

    HahRouter removido do index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Toaster } from "react-hot-toast";
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './services/firebase';
 import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
       <Toaster position="top-right" />
-    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
